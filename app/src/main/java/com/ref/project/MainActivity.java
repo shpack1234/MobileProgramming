002bc5c5314b38665177611ed3ca75a232b0b2cd
shpack1234 <shpack1234@naver.com>
2024-10-29 20:18:04 +0900

    TEST
    test

--- app/src/main/java/com/ref/project/MainActivity.java
+++ app/src/main/java/com/ref/project/MainActivity.java
@@ -1,6 +1,7 @@
 package com.ref.project;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,7 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Log.d("TEST", "commit test");
     }
 }
