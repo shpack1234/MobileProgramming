8fe77d60c9a9e9c1ccdf43488e240dc6d16e7e76
shpack1234 <shpack1234@naver.com>
2024-10-30 13:06:52 +0900

    2024.10.30
    Intro 화면 생성

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.ref.project"
-        minSdk = 26
+        minSdk = 28
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
