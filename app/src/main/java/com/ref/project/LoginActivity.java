09cb24e365c5cd3c71b71598fd3440156ec44cfa
Coppermine-SP <son5121502@gmail.com>
2024-11-12 20:27:25 +0900

    chore: resolve conflict after feature/login branch merge

--- app/src/main/java/com/ref/project/LoginActivity.java
+++ app/src/main/java/com/ref/project/LoginActivity.java
@@ -36,29 +36,6 @@ public class LoginActivity extends AppCompatActivity {
             return insets;
         });
 
-        // GoogleSignInOptions 설정
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.google_cloud_console_id))
-                .requestEmail()
-                .build();
-
-        silentSignIn();
-
-        // GoogleSignInClient 초기화
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-
-        // ActivityResultLauncher 초기화
-        googleSignInLauncher = registerForActivityResult(
-                new ActivityResultContracts.StartActivityForResult(),
-                result -> {
-                    if (result.getResultCode() == RESULT_OK) {
-                        Intent data = result.getData();
-                        Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-                        handleSignInResult(task.getResult());
-                    }
-                }
-        );
-
         // 로그인 버튼 클릭 이벤트
         SignInButton signInButton = findViewById(R.id.login_google_signIn_btn);
         signInButton.setOnClickListener(v -> onClickSignInBtn());
@@ -79,70 +56,15 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onResult(GetCredentialResponse getCredentialResponse) {
                 signInManager.SetAutoSignIn(true);
-        Log.d("Notion", "Login Start...");
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        googleSignInLauncher.launch(signInIntent); // ActivityResultLauncher 사용
-    }
-
-    // 로그인 결과 처리 메서드
-    private void handleSignInResult(GoogleSignInAccount account) {
-        try {
-            String idToken = account.getIdToken();
-
-            if (idToken != null) {
-                Log.d(TAG, "ID Token: " + idToken); // ID 토큰 로그
-                requestJWTToken(idToken); // JWT 토큰 요청
-
-                SharedPreferences prefs = getSharedPreferences("appPrefs", MODE_PRIVATE);
-                SharedPreferences.Editor editor = prefs.edit();
-                editor.putString("idToken", idToken);
-                editor.apply();
-
                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(intent);
             }
-        } catch (Exception e) {
-            Log.e(TAG, "signInResult:failed", e);
-        }
-    }
-
-
-    private void silentSignIn() {
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        if (account != null) {
-            Log.d(TAG, "자동 로그인 시도");
-            mGoogleSignInClient.silentSignIn().addOnCompleteListener(this, task -> {
-                if (task.isSuccessful()) {
-                    GoogleSignInAccount silentAccount = task.getResult();
 
             @Override
             public void onError(@NonNull GetCredentialException e) {
 
             }
         });
-                    if (silentAccount != null) {
-                        handleSignInResult(silentAccount);
-                    } else {
-                        Log.e(TAG, "silentSignIn: GoogleSignInAccount is null despite task success");
-                    }
-                } else {
-                    Log.d(TAG, "자동 로그인 실패, 수동 로그인 필요");
-                }
-            });
-        }
-    }
-
-
-    // 서버에 ID 토큰을 보내고 JWT 토큰을 요청하는 메서드
-    private void requestJWTToken(String idToken) {
-        OkHttpClient client = new OkHttpClient();
-
-        String url = "https://dev.cloudinteractive.net";
-
-        Request request = new Request.Builder()
-                .url(url)
-                .addHeader("Authorization", "Bearer " + idToken)
-                .build();
     }
 
 }
--- app/src/main/java/com/ref/project/MainActivity.java
+++ app/src/main/java/com/ref/project/MainActivity.java
@@ -2,12 +2,14 @@ package com.ref.project;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.credentials.CredentialManagerCallback;
 import androidx.credentials.exceptions.ClearCredentialException;
 import com.ref.project.Services.GoogleSignInManager;
@@ -15,9 +17,6 @@ import com.ref.project.Services.ServerAdapter;
 
 import javax.inject.Inject;
 import dagger.hilt.android.AndroidEntryPoint;
-
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.ref.project.data.ServerAdapter;
 
 @AndroidEntryPoint
 public class MainActivity extends AppCompatActivity {
@@ -31,10 +30,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
-        // 로그아웃 버튼 클릭 이벤트
-        Button logoutButton = findViewById(R.id.btn_logout);
-        logoutButton.setOnClickListener(v -> SignOut());
+        findViewById(R.id.main_signout_btn).setOnClickListener(v -> SignOut());
 
         // 백엔드 토큰 로그온
         serverAdapter.TokenSignIn(signInManager.GetIdToken(), new ServerAdapter.ITokenSignInCallback() {
@@ -55,40 +57,8 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        // 로그인된 ID 토큰 가져오기
-        SharedPreferences prefs = getSharedPreferences("appPrefs", MODE_PRIVATE);
-        String idToken = prefs.getString("idToken", null);
-        if (idToken != null) {
-            Log.d("MainActivity", "User is logged in with ID Token: " + idToken);
-        } else {
-            Log.d("MainActivity", "No user is logged in");
-        }
-
-        Button jwtButton = findViewById(R.id.btn_jwt);
-        jwtButton.setOnClickListener(v -> getjwt(idToken));
-
-        // 로그아웃 버튼 클릭 이벤트
-        //Button logoutButton = findViewById(R.id.btn_logout);
-        //logoutButton.setOnClickListener(v -> logout());
     }
 
-    private void getjwt(String idToken) {
-        ServerAdapter serverAdapter = ServerAdapter.getInstance(MainActivity.this);
-        serverAdapter.requestJWTToken(idToken, new ServerAdapter.JWTCallback() {
-            @Override
-            public void onSuccess(String jwtToken) {
-                Log.e("JWT SUCCESS", "토큰 발급 성공, 토큰 : "+ jwtToken);
-            }
-
-            @Override
-            public void onFailure(String errorMessage) {
-                Log.e("JWT FAIL", "토큰 발급 실패");
-            }
-        });
-
-    }
-
-    // 로그아웃 처리 메서드
     private void SignOut() {
         signInManager.SignOutAsync(this, new CredentialManagerCallback<Void, ClearCredentialException>() {
             @Override
