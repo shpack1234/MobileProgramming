43371fb24c8aaf92b2a354f601cee971f1dca8bb
copperminesp <son5121502@gmail.com>
2024-11-14 21:47:17 +0900

    WIP: AddItemsActivity

--- app/src/main/java/com/ref/project/Activities/AddItemsActivity.java
+++ app/src/main/java/com/ref/project/Activities/AddItemsActivity.java
@@ -1,16 +1,34 @@
 package com.ref.project.Activities;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.ref.project.R;
 
+import java.io.File;
+import java.io.IOException;
+
 public class AddItemsActivity extends AppCompatActivity {
+
+    private final String TAG = "AddItemsActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,5 +39,49 @@ public class AddItemsActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        findViewById(R.id.addItemsImportBtn).setOnClickListener(v -> importFromReceipt());
+    }
+
+    private File createImageFile() throws IOException {
+        String imageFileName = "temp";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+        return image;
+    }
+
+
+    private void importFromReceipt(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if(intent.resolveActivity(getPackageManager()) != null){
+            File file;
+            Uri fileUri;
+
+            try {
+                file = createImageFile();
+                fileUri = FileProvider.getUriForFile(getApplicationContext(), getPackageName(), file);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+
+                ActivityResultLauncher<Intent> result = registerForActivityResult(
+                        new ActivityResultContracts.StartActivityForResult(),
+                        new ActivityResultCallback<ActivityResult>() {
+                            @Override
+                            public void onActivityResult(ActivityResult o) {
+                                if(o.getResultCode() == Activity.RESULT_OK){
+
+                                }
+                            }
+                        });
+            }
+            catch(Exception e){
+                Log.e(TAG, "importFromReceipt Exception!\n" + e);
+            }
+        }
+        else
+        {
+            Log.w(TAG, "Camera resolveActivity failed!");
+        }
+
     }
 }
