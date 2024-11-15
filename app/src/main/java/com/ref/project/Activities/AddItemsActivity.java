0302cc69d765e81d5e33fafb458f91f997041bfc
copperminesp <son5121502@gmail.com>
2024-11-16 02:42:01 +0900

    design: AddItemsActivity

--- app/src/main/java/com/ref/project/Activities/AddItemsActivity.java
+++ app/src/main/java/com/ref/project/Activities/AddItemsActivity.java
@@ -1,11 +1,12 @@
 /*
-    AddItemsActivity- MPTeamProject
+    AddItemsActivity - MPTeamProject
     Copyright (C) 2024-2025 Coppermine-SP - <https://github.com/Coppermine-SP>.
  */
 package com.ref.project.Activities;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -15,8 +16,14 @@ import android.os.Handler;
 import android.os.Looper;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -31,8 +38,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.ref.project.Models.AddItemModel;
 import com.ref.project.Models.CategoryListModel;
 import com.ref.project.Models.CategoryModel;
+import com.ref.project.Models.ItemModel;
 import com.ref.project.Models.ReceiptItemModel;
 import com.ref.project.Models.ReceiptModel;
 import com.ref.project.R;
@@ -43,11 +52,90 @@ import com.ref.project.Views.TitleBar;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 import dagger.hilt.android.AndroidEntryPoint;
 import jakarta.inject.Inject;
+
+class ItemListDataAdapter extends BaseAdapter{
+    private final List<AddItemModel> items;
+    private final List<CategoryModel> categories;
+    private final Context context;
+    private final IActionHandler actionHandler;
+    private ViewHolder mViewHolder;
+
+    public ItemListDataAdapter(Context context, IActionHandler actionHandler, List<AddItemModel> items, List<CategoryModel> categories){
+        this.items = items;
+        this.categories = categories;
+        this.context = context;
+        this.actionHandler = actionHandler;
+    }
+
+    public static class ViewHolder {
+        private TextView categoryLabel;
+        private TextView descriptionLabel;
+        private TextView quantityLabel;
+        private TextView expiresLabel;
+        private ImageButton actionBtn;
+    }
+
+    public static interface IActionHandler{
+        public void onAction(int idx);
+    }
+
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+
+    @Override
+    public Object getItem(int pos) {
+        return items.get(pos);
+    }
+
+    @Override
+    public long getItemId(int pos) {
+        return pos;
+    }
+
+    @Override
+    public View getView(int pos, View convertView, ViewGroup parent) {
+        if(convertView == null){
+            convertView = LayoutInflater.from(context).inflate(R.layout.additem_item, parent, false);
+
+            mViewHolder = new ViewHolder();
+            mViewHolder.categoryLabel = convertView.findViewById(R.id.itemCategoryLabel);
+            mViewHolder.descriptionLabel = convertView.findViewById(R.id.itemDescriptionLabel);
+            mViewHolder.quantityLabel = convertView.findViewById(R.id.itemQuantityLabel);
+            mViewHolder.expiresLabel = convertView.findViewById(R.id.itemExpiresLabel);
+            mViewHolder.actionBtn = convertView.findViewById(R.id.itemActionBtn);
+            convertView.findViewById(R.id.itemActionBtn).setOnClickListener(v -> actionHandler.onAction(pos));
+
+            convertView.setTag(mViewHolder);
+        }
+        else{
+            mViewHolder = (ViewHolder)convertView.getTag();
+        }
+
+        AddItemModel item = items.get(pos);
+        String categoryName = categories.stream()
+                .filter(x -> x.CategoryId == item.CategoryId)
+                .map(x -> x.CategoryName)
+                .findFirst()
+                .orElse("null");
+
+        mViewHolder.categoryLabel.setText(categoryName);
+        mViewHolder.descriptionLabel.setText(item.ItemDescription);
+        mViewHolder.expiresLabel.setText(item.Expires.toString());
+        mViewHolder.quantityLabel.setText(String.format("%s개", String.valueOf(item.ItemQuantity)));
+
+        return convertView;
+    }
+}
 
 @AndroidEntryPoint
 public class AddItemsActivity extends AppCompatActivity {
@@ -60,10 +148,10 @@ public class AddItemsActivity extends AppCompatActivity {
     private Handler uiThreadHandler;
 
     private ListView itemsListView;
+    private View emptyPlaceholder;
     private List<CategoryModel> categories;
-
-    private ArrayList<String> list = new ArrayList<>();
-    private ArrayAdapter<String> adapter;
+    private List<AddItemModel> items;
+    private ItemListDataAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,6 +170,7 @@ public class AddItemsActivity extends AppCompatActivity {
         findViewById(R.id.addItemsAddBtn).setOnClickListener(v -> addNewRecord());
 
         itemsListView = findViewById(R.id.addItemsListView);
+        emptyPlaceholder = findViewById(R.id.addItemsEmpty);
         captureImageResultLauncher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
                 o -> {
@@ -96,7 +185,19 @@ public class AddItemsActivity extends AppCompatActivity {
                                 @Override
                                 public void onSuccess(ReceiptModel result) {
                                     for(ReceiptItemModel x : result.Items) {
-                                        list.add(x.ItemDescription + " - " + x.ItemQuantity + "개");
+                                        AddItemModel model = new AddItemModel();
+                                        model.CategoryId = x.CategoryId;
+                                        model.ItemDescription = x.ItemDescription;
+                                        model.ItemQuantity = x.ItemQuantity;
+
+                                        int defaultExpires = categories.stream()
+                                                        .filter(y -> y.CategoryId == x.CategoryId)
+                                                        .map(y -> y.RecommendedExpirationDays)
+                                                        .findFirst()
+                                                        .orElse(30);
+
+                                        model.Expires = LocalDate.now().plusDays(defaultExpires);
+                                        addItem(model);
                                     }
 
                                     uiThreadHandler.post(() -> adapter.notifyDataSetChanged());
@@ -121,6 +222,9 @@ public class AddItemsActivity extends AppCompatActivity {
             @Override
             public void onSuccess(CategoryListModel result) {
                 categories = result.Categories;
+                items = new ArrayList<AddItemModel>();
+                adapter = new ItemListDataAdapter(AddItemsActivity.this, (x -> itemAction(x)), items, categories);
+                uiThreadHandler.post(() -> itemsListView.setAdapter(adapter));
             }
 
             @Override
@@ -133,13 +237,31 @@ public class AddItemsActivity extends AppCompatActivity {
         });
     }
 
-    //region
-
-    private void addNewRecord(){
-        ImportLoadingDialog dialog = new ImportLoadingDialog();
-        dialog.show(getSupportFragmentManager(), "importLoadingDialog");
+    //region itemList behaviour
+    private void addItem(AddItemModel x){
+        emptyPlaceholder.setVisibility(View.INVISIBLE);
+        items.add(x);
     }
 
+    private void deleteItem(int idx){
+        items.remove(idx);
+        if(items.isEmpty()) emptyPlaceholder.setVisibility(View.VISIBLE);
+    }
+
+    private void itemAction(int idx){
+        Toast.makeText(this, String.valueOf(idx), Toast.LENGTH_LONG).show();
+    }
+
+    private void addNewRecord(){
+        AddItemModel model = new AddItemModel();
+        model.ItemQuantity = 5;
+        model.ItemDescription = "테스트 아이탬";
+        model.Expires = LocalDate.now();
+        model.CategoryId = 7;
+
+        addItem(model);
+        adapter.notifyDataSetChanged();
+    }
     //endregion
 
 
